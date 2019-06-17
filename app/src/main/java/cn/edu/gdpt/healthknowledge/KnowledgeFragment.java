cd22948783e124690b0846da2f7dd9ccd4c8804b
admin <892557365@qq.com>
2019-06-17 15:46:59 +0800

    健康小知识

--- app/src/main/java/cn/edu/gdpt/healthknowledge/KnowledgeFragment.java
+++ app/src/main/java/cn/edu/gdpt/healthknowledge/KnowledgeFragment.java
@@ -37,25 +37,26 @@ public class KnowledgeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       // return inflater.inflate(R.layout.fragment_knowledge, container, false);
-        View view=inflater.inflate(R.layout.fragment_knowledge,container,false);
-        bmb=(BoomMenuButton)view.findViewById(R.id.bmb);
+        // return inflater.inflate(R.layout.fragment_knowledge, container, false);
+        View view = inflater.inflate(R.layout.fragment_knowledge, container, false);
+        bmb = (BoomMenuButton) view.findViewById(R.id.bmb);
         assert bmb != null;
 
         bmb.setButtonEnum(ButtonEnum.TextInsideCircle);
         bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_9_1);
 
         bmb.setButtonPlaceEnum(ButtonPlaceEnum.SC_9_1);
-        for (int i=0;i<bmb.getPiecePlaceEnum().pieceNumber();i++){
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
             addBuilder();
         }
-        webView = (WebView)view.findViewById(R.id.webview);
+        webView = (WebView) view.findViewById(R.id.webview);
         //HealthRVadapter healthRVadapter=new HealthRVadapter(getActivity(),KnowledgeBean);
         // webView.setAdapter(healthRVadapter);
         return view;
 
     }
-    private void addBuilder(){
+
+    private void addBuilder() {
 
 
         bmb.addBuilder(new TextInsideCircleButton.Builder()
@@ -65,14 +66,71 @@ public class KnowledgeFragment extends Fragment {
                 .listener(new OnBMClickListener() {
                     @Override
                     public void onBoomButtonClick(int index) {
-                        switch (index){
+                        switch (index) {
                             case 0:
-                                Uri uri = Uri.parse("http://www.baidu.com");
-                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                                startActivity(intent);
+                                Intent intent1 = new Intent(getActivity(), ShowActivity.class);
+                                intent1.putExtra("URL", "https://baike.baidu.com/item/%E8%90%A5%E5%85%BB%E7%B4%A0");
+                                startActivity(intent1);
                                 //webView = (WebView) findViewById(R.id.webView);
                                 //webView.loadUrl("http://baidu.com");
                                 break;
+                            case 1:
+                                Intent intent2 = new Intent(getActivity(), ShowActivity.class);
+                                intent2.putExtra("URL", "https://jingyan.baidu.com/article/0bc808fc884fe11bd485b90e.html");
+                                startActivity(intent2);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+                            case 2:
+                                Intent intent3 = new Intent(getActivity(), ShowActivity.class);
+                                intent3.putExtra("URL", "https://baike.baidu.com/item/%E5%BF%83%E7%90%86%E5%81%A5%E5%BA%B7/5593?fr=aladdin");
+                                startActivity(intent3);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+                            case 3:
+                                Intent intent4 = new Intent(getActivity(), ShowActivity.class);
+                                intent4.putExtra("URL", "https://baijiahao.baidu.com/s?id=1571337126264904&wfr=spider&for=pc");
+                                startActivity(intent4);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+                            case 4:
+                                Intent intent5 = new Intent(getActivity(), ShowActivity.class);
+                                intent5.putExtra("URL", "http://www.360doc.com/content/18/0603/01/40501103_759213919.shtml");
+                                startActivity(intent5);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+                            case 5:
+                                Intent intent6 = new Intent(getActivity(), ShowActivity.class);
+                                intent6.putExtra("URL", "https://baike.baidu.com/item/%E9%A3%9F%E5%93%81%E5%AE%89%E5%85%A8/6173?fr=aladdin");
+                                startActivity(intent6);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+                            case 6:
+                                Intent intent7 = new Intent(getActivity(), ShowActivity.class);
+                                intent7.putExtra("URL", "https://baike.baidu.com/item/%E5%81%A5%E5%BA%B7%E9%A5%AE%E9%A3%9F%E4%B9%A0%E6%83%AF/2314752?fr=aladdin");
+                                startActivity(intent7);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+                            case 7:
+                                Intent intent8 = new Intent(getActivity(), ShowActivity.class);
+                                intent8.putExtra("URL", "https://baike.baidu.com/item/%E4%BF%9D%E5%81%A5/991482?fr=aladdin");
+                                startActivity(intent8);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+                            case 8:
+                                Intent intent9 = new Intent(getActivity(), ShowActivity.class);
+                                intent9.putExtra("URL", "https://baike.baidu.com/item/%E8%BF%90%E5%8A%A8/2134938#viewPageContent");
+                                startActivity(intent9);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+
 
 
                         }
--- app/src/main/java/cn/edu/gdpt/healthknowledge/ShowActivity.java
+++ app/src/main/java/cn/edu/gdpt/healthknowledge/ShowActivity.java
@@ -2,12 +2,36 @@ package cn.edu.gdpt.healthknowledge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class ShowActivity extends AppCompatActivity {
+
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        //Toast.makeText(this, "asdasdasd", Toast.LENGTH_SHORT).show();
+
+        if (getIntent() != null) {
+            url = getIntent().getStringExtra("URL");
+        }
+        final WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+        webView.loadUrl(url);
+
+
     }
 }
