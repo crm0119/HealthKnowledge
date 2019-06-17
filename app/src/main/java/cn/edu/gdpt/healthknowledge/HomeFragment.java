11d0b85ab72498430b698d44307f7be36ecdb024
admin <892557365@qq.com>
2019-06-17 14:24:49 +0800

    首页实体类

--- app/src/main/java/cn/edu/gdpt/healthknowledge/HomeFragment.java
+++ app/src/main/java/cn/edu/gdpt/healthknowledge/HomeFragment.java
@@ -1,17 +1,39 @@
 package cn.edu.gdpt.healthknowledge;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    private Activity activity;
+    private ViewGroup viewGroup;
+    private EditText edt;
+    private Button btn;
+    private RecyclerView recyclerView;
+    private List<HomeBean.ResultBean.ListBean> listBeans;
+    private Gson gson=new Gson();
+    private TextView item_tv_content;
+    private Layout itemlist;
+
 
 
     public HomeFragment() {
@@ -23,7 +45,55 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+       // return inflater.inflate(R.layout.fragment_home, container, false);
+        activity = getActivity();
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        initView(viewGroup);
+
+
+        viewGroup.findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(activity, "button", Toast.LENGTH_SHORT).show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            final String sync_get = HttpUtils.Sync_Get(edt.getText().toString().trim());
+                            HomeBean bean=gson.fromJson(sync_get,HomeBean.class);
+                            listBeans=bean.getResult().getList();
+                            activity.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    // Toast.makeText(activity, sync_get, Toast.LENGTH_SHORT).show();
+                                    HealthRVadapter healthRVadapter=new HealthRVadapter(getActivity(),listBeans);
+                                    LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+                                    linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                                    recyclerView.setLayoutManager(linearLayoutManager);
+                                    recyclerView.setAdapter(healthRVadapter);
+
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+
+        return viewGroup;
     }
+
+    private void initView(View viewGroup) {
+        edt = (EditText) viewGroup.findViewById(R.id.edt);
+        recyclerView=(RecyclerView) viewGroup.findViewById(R.id.health_recycler_view);
+        item_tv_content=(TextView)viewGroup.findViewById(R.id.item_tv_content);
+
+
+    }
+
 
 }
--- app/src/main/java/cn/edu/gdpt/healthknowledge/KnowledgeFragment.java
+++ app/src/main/java/cn/edu/gdpt/healthknowledge/KnowledgeFragment.java
@@ -1,17 +1,31 @@
 package cn.edu.gdpt.healthknowledge;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
+import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class KnowledgeFragment extends Fragment {
+    private BoomMenuButton bmb;
+    private WebView webView;
+    private Context context;
 
 
     public KnowledgeFragment() {
@@ -23,7 +37,48 @@ public class KnowledgeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_knowledge, container, false);
+       // return inflater.inflate(R.layout.fragment_knowledge, container, false);
+        View view=inflater.inflate(R.layout.fragment_knowledge,container,false);
+        bmb=(BoomMenuButton)view.findViewById(R.id.bmb);
+        assert bmb != null;
+
+        bmb.setButtonEnum(ButtonEnum.TextInsideCircle);
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_9_1);
+
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.SC_9_1);
+        for (int i=0;i<bmb.getPiecePlaceEnum().pieceNumber();i++){
+            addBuilder();
+        }
+        webView = (WebView)view.findViewById(R.id.webview);
+        //HealthRVadapter healthRVadapter=new HealthRVadapter(getActivity(),KnowledgeBean);
+        // webView.setAdapter(healthRVadapter);
+        return view;
+
     }
+    private void addBuilder(){
+
+
+        bmb.addBuilder(new TextInsideCircleButton.Builder()
+
+                .normalImageRes(BuilderManager.getImageResource())
+                .normalTextRes(BuilderManager.getTextResource())
+                .listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+                        switch (index){
+                            case 0:
+                                Uri uri = Uri.parse("http://www.baidu.com");
+                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                startActivity(intent);
+                                //webView = (WebView) findViewById(R.id.webView);
+                                //webView.loadUrl("http://baidu.com");
+                                break;
+
+
+                        }
+                    }
+                }));
+    }
+
 
 }
