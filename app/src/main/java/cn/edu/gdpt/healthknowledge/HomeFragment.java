2fc343df3ccaf50d9dec7f675e98319a0da975dd
www.1911644586@qq.com <www.1911644586@qq.com>
2019-06-21 10:33:26 +0800

    首页文本框优化

--- app/src/main/java/cn/edu/gdpt/healthknowledge/HomeFragment.java
+++ app/src/main/java/cn/edu/gdpt/healthknowledge/HomeFragment.java
@@ -8,12 +8,14 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -76,21 +78,31 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void run() {
                         try {
-                            final String sync_get = HttpUtils.Sync_Get(edt.getText().toString().trim());
-                            HomeBean bean=gson.fromJson(sync_get,HomeBean.class);
-                            listBeans=bean.getResult().getList();
-                            activity.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    // Toast.makeText(activity, sync_get, Toast.LENGTH_SHORT).show();
-                                    HealthRVadapter healthRVadapter=new HealthRVadapter(getActivity(),listBeans);
-                                    LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
-                                    linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-                                    recyclerView.setLayoutManager(linearLayoutManager);
-                                    recyclerView.setAdapter(healthRVadapter);
+                            if(!TextUtils.isEmpty(edt.getText().toString().trim())){
+                                final String sync_get = HttpUtils.Sync_Get(edt.getText().toString().trim());
+                                HomeBean bean=gson.fromJson(sync_get,HomeBean.class);
+                                listBeans=bean.getResult().getList();
+                                activity.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Toast.makeText(activity, sync_get, Toast.LENGTH_SHORT).show();
+                                        HealthRVadapter healthRVadapter=new HealthRVadapter(getActivity(),listBeans);
+                                        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+                                        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                                        recyclerView.setLayoutManager(linearLayoutManager);
+                                        recyclerView.setAdapter(healthRVadapter);
 
-                                }
-                            });
+                                    }
+                                });
+                            }else {
+                                activity.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(activity, "输入不能为空", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                            
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -113,21 +125,34 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void run() {
                         try {
-                            final String sync_get = HttpUtils.Sync_Get(edt.getText().toString().trim());
-                            HomeBean bean=gson.fromJson(sync_get,HomeBean.class);
-                            listBeans=bean.getResult().getList();
-                            activity.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    // Toast.makeText(activity, sync_get, Toast.LENGTH_SHORT).show();
-                                    HealthRVadapter healthRVadapter=new HealthRVadapter(getActivity(),listBeans);
-                                    LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
-                                    linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-                                    recyclerView.setLayoutManager(linearLayoutManager);
-                                    recyclerView.setAdapter(healthRVadapter);
+                            if (!TextUtils.isEmpty(edt.getText().toString().trim())){
+                                final String sync_get = HttpUtils.Sync_Get(edt.getText().toString().trim());
+                                HomeBean bean=gson.fromJson(sync_get,HomeBean.class);
+                                listBeans=bean.getResult().getList();
+                                activity.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Toast.makeText(activity, sync_get, Toast.LENGTH_SHORT).show();
+                                        HealthRVadapter healthRVadapter=new HealthRVadapter(getActivity(),listBeans);
+                                        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+                                        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                                        recyclerView.setLayoutManager(linearLayoutManager);
+                                        recyclerView.setAdapter(healthRVadapter);
 
-                                }
-                            });
+                                    }
+                                });
+                            }else {
+                                activity.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(activity, "输入不能为空", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+
+
+
+
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
